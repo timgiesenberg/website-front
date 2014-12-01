8a22582fea51f0b7e7c8d3f1251240db9b178c12
Piotr Pałek <piotr.palek@netguru.pl>
2014-12-01 01:20:26 +0100

    add pod structure config

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'etl',
+    podModulePrefix: 'etl/pods',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
